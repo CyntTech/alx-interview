297de18292c74915d97ea8cef7edd7cb0eeda959
Cynthia Amaji <cynthyok4luv@gmail.com>
2023-04-03 01:53:24 +0100

    perimeter of an island

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,21 +1,21 @@
- #!/usr/bin/python3
- """
- Perimeter of an Island
- """
+#!/usr/bin/python3
+"""
+Perimeter of an Island
+"""
 
- 
- def island_perimeter(grid):
-     """
-     A function that determines the perimeter of an island described 
-     in a list of integers.
-     """
-     perimeter = 0
-     for i in range(len(grid)):
-         for j in range(len(grid[i])):
-             if grid[i][j] == 1:
-                 perimeter += 4
-                 if (i > 0 and grid[i - 1][j] == 1):
-                     perimeter -= 2
-                 if (j > 0 and grid[i][j - 1] == 1):
-                     perimeter -= 2
-     return perimeter
+
+def island_perimeter(grid):
+  """
+  A function that determines the perimeter of an island described 
+  in a list of integers.
+  """
+  perimeter = 0
+  for i in range(len(grid)):
+      for j in range(len(grid[i])):
+          if grid[i][j] == 1:
+              perimeter += 4
+              if (i > 0 and grid[i - 1][j] == 1):
+                  perimeter -= 2
+              if (j > 0 and grid[i][j - 1] == 1):
+                  perimeter -= 2
+  return perimeter
